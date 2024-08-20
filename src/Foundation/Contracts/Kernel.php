7a629456f1385b7d6ea188949f30da95884688ef
Abdulbasit <basttyy@gmail.com>
2024-08-20 23:06:41 +0100

    fix paths

--- src/Foundation/Contracts/Kernel.php
+++ src/Foundation/Contracts/Kernel.php
@@ -4,37 +4,5 @@ namespace Eyika\Atom\Framework\Foundation\Contracts;
 
 interface Kernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array
-     */
-    public $middleware;
 
-    /**
-     * The application's route middleware groups.
-     *
-     * @var array
-     */
-    public $middlewareGroups;
-
-    /**
-     * The application's middleware aliases.
-     *
-     * Aliases may be used to conveniently assign middleware to routes and groups.
-     *
-     * @var array
-     */
-    public $middlewareAliases;
-
-    /**
-     * The priority-sorted list of middleware.
-     *
-     * This forces non-global middleware to always be in the given order.
-     *
-     * @var array
-     */
-    public $middlewarePriority;
 }
