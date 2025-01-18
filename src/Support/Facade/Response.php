7bbbb622106547a9fede4789742a3b833f2ab1ef
Abdulbasit <basttyy@gmail.com>
2025-01-18 16:42:29 +0100

    restructure json response class

--- src/Support/Facade/Response.php
+++ src/Support/Facade/Response.php
@@ -8,7 +8,7 @@ use Eyika\Atom\Framework\Http\Response as HttpResponse;
  * @method static HttpResponse getInstance()
  * @method static HttpResponse plain(string $message, int $statusCode = self::STATUS_OK)
  * @method static HttpResponse html(string $message, int $statusCode = self::STATUS_OK)
- * @method static HttpResponse json(array|string $message, array|int $dataOrStatus = self::STATUS_OK, int|null $statusCode = null)
+ * @method static HttpResponse json(array $data, int $statusCode = self::STATUS_OK)
  * @method static HttpResponse view(string $file_name, array $data = [])
  * @method static HttpResponse redirect(string $to, int $code = self::STATUS_FOUND, int|null $delay = null)
  * @method static HttpResponse back(int $code = self::STATUS_SEE_OTHER, int|null $delay = null)
