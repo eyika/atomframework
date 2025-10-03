121d4c3a21a0ff5c91ed88e5c35d0475cca17829
Abdulbasit <basttyy@gmail.com>
2025-10-03 14:47:15 +0100

    fix query builder save

--- src/Foundation/ExceptionHandler.php
+++ src/Foundation/ExceptionHandler.php
@@ -174,7 +174,7 @@ class ExceptionHandler implements ContractExceptionHandler
 
     protected function renderErrorPage(Request $request, Throwable $exception): BaseResponse
     {
-        if (config('app.debug', true)) {
+        if (config('app.debug', 'false') == 'true') {
             $whoops = new \Whoops\Run;
             $whoops->allowQuit(false);
             $whoops->writeToOutput(false);
